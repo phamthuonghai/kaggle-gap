e79cf44e8b2deb2d6f40a2fd6671cefbeb6c4cd3
Thuong-Hai Pham <phamthuonghai@gmail.com>
2019-03-18 17:33:58 +0700

    visualize enc attentions notebook

--- compute_embs.py
+++ compute_embs.py
@@ -4,31 +4,7 @@ import time
 import numpy as np
 import pandas as pd
 
-
-def compute_offset_no_spaces(text, offset):
-    count = 0
-    for pos in range(offset):
-        if text[pos] != ' ':
-            count += 1
-    return count
-
-
-def count_chars_no_special(text):
-    count = 0
-    special_char_list = ['#']
-    for pos in range(len(text)):
-        if text[pos] not in special_char_list:
-            count += 1
-    return count
-
-
-def count_length_no_special(text):
-    count = 0
-    special_char_list = ['#', ' ']
-    for pos in range(len(text)):
-        if text[pos] not in special_char_list:
-            count += 1
-    return count
+from utils import compute_offset_no_spaces, count_length_no_special
 
 
 def run_bert(data_name):
